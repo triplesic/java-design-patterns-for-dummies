bf7d2584697e99fa82dd25589346a036ecf09acd
Triplesic <triplesic@gmail.com>
2017-04-19 17:42:57 +0700

    Create Pepsi.java

---
+++ builder/src/main/java/org/triplesic/builder/item/colddrink/Pepsi.java
@@ -0,0 +1,15 @@
+package org.triplesic.builder.item.colddrink;
+
+public class Pepsi extends ColdDrink {
+
+	@Override
+	public float price() {
+		return 35.0f;
+	}
+
+	@Override
+	public String name() {
+		return "Pepsi";
+	}
+
+}
