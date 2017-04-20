c7626750f0a39c2e110c166bb901a712256fcb3f
Triplesic <triplesic@gmail.com>
2017-04-20 13:28:49 +0700

    Update Meal.java

--- builder/src/main/java/org/triplesic/builder/Meal.java
+++ builder/src/main/java/org/triplesic/builder/Meal.java
@@ -1,11 +1,24 @@
 package org.triplesic.builder;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import org.triplesic.builder.item.Item;
+import org.triplesic.builder.item.burger.ChickenBurger;
+import org.triplesic.builder.item.burger.VegBurger;
+import org.triplesic.builder.item.colddrink.Coke;
+import org.triplesic.builder.item.colddrink.Pepsi;
 
-public class Meal {
+public final class Meal {
 	private List<Item> items = new ArrayList<Item>();
+
+	public Meal() {
+
+	}
+
+	private Meal(Builder builder) {
+		this.items = builder.items;
+	}
 
 	public void addItem(Item item) {
 		items.add(item);
@@ -28,4 +41,47 @@ public class Meal {
 			System.out.println(", Price : " + item.price());
 		}
 	}
+
+	public static class Builder {
+		private List<Item> items = new ArrayList<Item>();
+
+		public Builder() {
+		}
+
+		public Builder buildVegMeal() {
+			this.items.add(new VegBurger());
+			this.items.add(new Coke());
+			return this;
+		}
+
+		public Builder buildNonVegMeal() {
+			this.items.add(new ChickenBurger());
+			this.items.add(new Pepsi());
+			return this;
+		}
+
+		public Builder addVegBurger(){
+			this.items.add(new VegBurger());
+			return this;
+		}
+		
+		public Builder addChickenBurger(){
+			this.items.add(new ChickenBurger());
+			return this;
+		}
+		public Builder addCoke(){
+			this.items.add(new Coke());
+			return this;
+		}
+		
+		public Builder addPepsi(){
+			this.items.add(new Pepsi());
+			return this;
+		}
+		
+		public Meal build() {
+			return new Meal(this);
+		}
+	}
+
 }
